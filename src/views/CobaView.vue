f36286aded4a3f1afe8dd89a7e69e83af302d1db
Yzerhaf04 <teuku.rafli.2104@gmail.com>
2025-10-03 07:25:14 +0700

    add submodules

---
+++ src/views/CobaView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="coba">
+        <h1>Ini adalah page coba</h1>
+    </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    font-size: 32px; /* Added font size */
+    color: red;      /* Added font color */
+  }
+}
+</style>
